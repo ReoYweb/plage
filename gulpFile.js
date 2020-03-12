248ae0b1ec1b21a81c269edfdb635626766dd53e
Reo <yoshioka.web.d@gmail.com>
2020-03-12 15:17:14 +0900

    gulp

---
+++ gulpFile.js
@@ -0,0 +1,37 @@
+
+// gulp scss
+
+// npm init -y
+// npm install -D gulp
+// npm install -D gulp gulp-sass
+
+// 実行 npx gulp
+
+// gulpプラグインを読み込みます
+const { src, dest, watch } = require("gulp");
+// Sassをコンパイルするプラグインを読み込みます
+const sass = require("gulp-sass");
+
+/**
+ * Sassをコンパイルするタスクです
+ */
+const compileSass = () =>
+  // style.scssファイルを取得
+  src("scss/main.scss")
+    // Sassのコンパイルを実行
+    .pipe(
+      // コンパイル後のCSSを展開
+      sass({
+        outputStyle: "compressed"
+      })
+    )
+    // cssフォルダー以下に保存
+    .pipe(dest("css"));
+
+/**
+ * Sassファイルを監視し、変更があったらSassを変換します
+ */
+const watchSassFiles = () => watch("scss/main.scss", compileSass);
+
+// npx gulpというコマンドを実行した時、watchSassFilesが実行されるようにします
+exports.default = watchSassFiles;
